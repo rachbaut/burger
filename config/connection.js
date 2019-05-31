42dc716e7486d17b5bba167ce71ded0689e4aa31
Rachel Bautista <rachelbaut5@gmail.com>
2019-05-30 20:14:49 -0700

    changes made to host

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection= mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
+    host : '127.0.0.1',
+    port: 3306,
+    user : 'root',
     password : '',
     database : 'burgers_db',
-    socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock'
   });
 }
 
